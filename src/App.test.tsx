bd3386129ce09e9fafc9682db4804dfdb7d240a7
Juan Trejos <juan.trejos@ccorporation.us>
2023-01-16 12:06:47 -0500

    Clean project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { test, expect } from "jest";
 import App from "./App";
 
 test("renders learn react link", () => {
